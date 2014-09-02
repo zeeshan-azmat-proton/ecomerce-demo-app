3c0b99a431787b9bc8674660fdef71a9e403453c
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2014-09-02 12:47:10 +0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcomerceDemoApp::Application.config.secret_token = 'db50720cb68071c8c354f6b5c03965fc77a4d2f6f0d107cf19c239cc056b6bb182880baeee186e06f1e97d7416549cdfd4f755ff42d5e67e21bdbe9bdcf4ae71'
